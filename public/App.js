0c37200c4d7856fc9a48855fb513ea44a6e52b9a
HsuanYu <syuanyu@hotmail.com.tw>
2020-02-11 20:14:49 -0800

    Completed the most of codes. It hasn't reviewed.

--- public/App.js
+++ public/App.js
@@ -24,22 +24,28 @@ class InventorySubhead extends React.Component {
 const initialProducts = [{
   id: 1,
   name: 'Blue Shirt',
-  price: '$16.99',
+  price: '16.99',
   category: 'Shirts',
-  image_url: 'View'
+  image_url: 'https://images.app.goo.gl/A1VVdgNYDBFprrow5'
 }, {
   id: 2,
   name: 'Logo Hat',
-  price: '$12.99',
+  price: '12.99',
   category: 'Accessories',
-  image_url: 'View'
+  image_url: 'https://images.app.goo.gl/bBjLavbRvs7DJtpu8'
+}, {
+  id: 3,
+  name: 'Regular Fit Jeans',
+  price: '34.99',
+  category: 'Jeans',
+  image_url: 'https://images.app.goo.gl/ALG2aDEKpPxGV9137'
 }];
 
 function ProductRow(props) {
   const product = props.product;
   return React.createElement("tr", null, React.createElement("td", {
     id: "body_pro_id"
-  }, product.id), React.createElement("td", null, product.name), React.createElement("td", null, product.price), React.createElement("td", null, product.category), React.createElement("td", null, React.createElement("a", {
+  }, product.id), React.createElement("td", null, product.name), React.createElement("td", null, '$' + product.price), React.createElement("td", null, product.category), React.createElement("td", null, React.createElement("a", {
     href: product.image_url,
     target: "_blank"
   }, "View")));
@@ -57,20 +63,103 @@ function ProductTable(props) {
   }, "ID"), React.createElement("th", null, "Name"), React.createElement("th", null, "Price"), React.createElement("th", null, "Category"), React.createElement("th", null, "Image"))), React.createElement("tbody", null, productRows));
 }
 
+class ProductAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.productAdd;
+    var price = form.priceper.value;
+    price = price.replace('$', ''); // alert(price);
+
+    const product = {
+      name: form.name.value,
+      category: form.category.value,
+      price: price,
+      image_url: form.image_url.value
+    };
+    this.props.createProduct(product); // form.category.selectedIndex  = -1;
+    // form.priceper.value = "$";
+    // form.name.value = "";
+    // form.image_url.value = "";
+
+    form.reset();
+  }
+
+  render() {
+    return React.createElement("form", {
+      name: "productAdd",
+      onSubmit: this.handleSubmit
+    }, React.createElement("div", {
+      class: "grid_container"
+    }, React.createElement("div", null, React.createElement("label", null, "Category"), React.createElement("br", null), React.createElement("select", {
+      type: "text",
+      name: "category",
+      selectedIndex: 0
+    }, React.createElement("option", {
+      value: "Shirts"
+    }, "Shirts"), React.createElement("option", {
+      value: "Jeans"
+    }, "Jeans"), React.createElement("option", {
+      value: "Jackets"
+    }, "Jackets"), React.createElement("option", {
+      value: "Sweaters"
+    }, "Sweaters"), React.createElement("option", {
+      value: "Accessories"
+    }, "Accessories"))), React.createElement("div", null, React.createElement("label", null, "Price Per Unit"), React.createElement("br", null), React.createElement("input", {
+      type: "text",
+      name: "priceper",
+      defaultValue: '$'
+    })), React.createElement("div", null, React.createElement("label", null, "Product Name"), React.createElement("br", null), React.createElement("input", {
+      type: "text",
+      name: "name"
+    })), React.createElement("div", null, React.createElement("label", null, "Image URL"), React.createElement("br", null), React.createElement("input", {
+      type: "text",
+      name: "image_url"
+    }))), React.createElement("br", null), React.createElement("button", null, "Add Product"));
+  }
+
+}
+
 class MyProductList extends React.Component {
   constructor() {
     super();
     this.state = {
+      products: []
+    };
+    this.createProduct = this.createProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.setState({
       products: initialProducts
-    }; // this.createIssue = this.createIssue.bind(this);
+    });
+  }
+
+  createProduct(product) {
+    product.id = this.state.products.length + 1;
+    const newProductList = this.state.products.slice();
+    newProductList.push(product);
+    this.setState({
+      products: newProductList
+    });
   }
 
   render() {
     const head = "My Company Inventory";
     const addhead = "Add a new product to inventory";
-    return React.createElement(React.Fragment, null, React.createElement("h1", null, head), React.createElement(InventorySubhead, null), React.createElement("hr", null), React.createElement(ProductTable, {
+    return React.createElement(React.Fragment, null, React.createElement("h1", null, head), React.createElement(InventorySubhead, null), React.createElement("hr", null), React.createElement("br", null), React.createElement(ProductTable, {
       products: this.state.products
-    }), React.createElement("label", null, addhead), React.createElement("hr", null));
+    }), React.createElement("br", null), React.createElement("label", null, addhead), React.createElement("hr", null), React.createElement(ProductAdd, {
+      createProduct: this.createProduct
+    }));
   }
 
 }
--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,16 @@ class InventorySubhead extends React.Component {
 
 const initialProducts = [
     {
-      id: 1, name: 'Blue Shirt',price: '$16.99', category: 'Shirts', image_url: 'View',
+        id: 1, name: 'Blue Shirt',price: '16.99', 
+        category: 'Shirts', image_url: 'https://images.app.goo.gl/A1VVdgNYDBFprrow5',
     },
     {
-        id: 2, name: 'Logo Hat',price: '$12.99', category: 'Accessories', image_url: 'View',
+        id: 2, name: 'Logo Hat',price: '12.99',
+        category: 'Accessories', image_url: 'https://images.app.goo.gl/bBjLavbRvs7DJtpu8',
+    },
+    {
+        id: 3, name: 'Regular Fit Jeans',price: '34.99',
+        category: 'Jeans', image_url: 'https://images.app.goo.gl/ALG2aDEKpPxGV9137',
     },
   ];
 
@@ -41,7 +47,7 @@ function ProductRow(props) {
       <tr>
         <td id='body_pro_id'>{product.id}</td>
         <td>{product.name}</td>
-        <td>{product.price}</td>
+        <td>{'$'+product.price}</td>
         <td>{product.category}</td>
         <td><a href={product.image_url} target="_blank">View</a></td>
       </tr>
@@ -68,31 +74,106 @@ function ProductTable(props) {
         </tbody>
       </table>
     );
+}
+
+class ProductAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.productAdd;
+    var price = form.priceper.value;
+    price = price.replace('$','');
+    // alert(price);
+    const product = {
+      name: form.name.value, category: form.category.value,
+      price: price, image_url: form.image_url.value
+    };
+    this.props.createProduct(product);
+    // form.category.selectedIndex  = -1;
+    // form.priceper.value = "$";
+    // form.name.value = "";
+    // form.image_url.value = "";
+    form.reset();
+  }
+  render() {
+    return (
+      <form name="productAdd" onSubmit={this.handleSubmit}>
+        <div class="grid_container">
+          <div>
+            <label>Category</label>
+            <br/>
+            <select type="text" name="category" selectedIndex={0}>
+              <option value='Shirts'>Shirts</option>
+              <option value='Jeans'>Jeans</option>
+              <option value='Jackets'>Jackets</option>
+              <option value='Sweaters'>Sweaters</option>
+              <option value='Accessories'>Accessories</option>
+            </select>
+          </div>
+          <div>
+            <label>Price Per Unit</label>
+            <br/>
+            <input type="text" name="priceper" defaultValue={'$'}/>
+          </div>
+          <div>
+            <label>Product Name</label>
+            <br/>
+            <input type="text" name="name"/>
+          </div>
+          <div>
+            <label>Image URL</label>
+            <br/>
+            <input type="text" name="image_url"/>
+          </div>
+        </div>
+        <br/>
+        <button>Add Product</button>
+      </form>
+    );
+  }
+}
 
 
 class MyProductList extends  React.Component{
-    constructor() {
-        super();
-        this.state = { products: initialProducts };
-        // this.createIssue = this.createIssue.bind(this);
-      }
+  constructor() {
+    super();
+    this.state = { products: [] };
+    this.createProduct = this.createProduct.bind(this);
+  }
 
-    render(){
-        const head = "My Company Inventory";
-        const addhead = "Add a new product to inventory";
-        return(
-            <React.Fragment>
-                <h1>{head}</h1>
-                <InventorySubhead/>
-                <hr/>
-                <ProductTable products={this.state.products}/>
-                <label>{addhead}</label>
-                <hr/>
-                {/* ProductAdd */}
-            </React.Fragment>
-        )
-    }
+  componentDidMount() {
+    this.loadData();
+  }
+  loadData() {
+    this.setState({ products: initialProducts });
+  }
+  createProduct(product) {
+    product.id = this.state.products.length + 1;
+    const newProductList = this.state.products.slice();
+    newProductList.push(product);
+    this.setState({ products: newProductList });
+  }
+
+  render(){
+    const head = "My Company Inventory";
+    const addhead = "Add a new product to inventory";
+    return(
+        <React.Fragment>
+            <h1>{head}</h1>
+            <InventorySubhead/>
+            <hr/>
+            <br/>
+            <ProductTable products={this.state.products}/>
+            <br/>
+            <label>{addhead}</label>
+            <hr/>
+            <ProductAdd createProduct = {this.createProduct}/>
+        </React.Fragment>
+    )
+  }
 }
 
 const element =<MyProductList/>;
